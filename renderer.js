5b1e9a8ce692247fb2e3191b54a027dbced957cb
Gui P <guillaume@guillaumepasquet.fr>
2016-08-05 16:56:44 +0200

    yeah, started implementing images resizing

--- renderer.js
+++ renderer.js
@@ -2,7 +2,13 @@ const angular = require('angular');
 
 class AppController {
   $onInit() {
-    this.options = {};
+    this.options = {
+      location: "auto",
+      output_folder: false,
+      ratio: "1",
+      rotation: "0",
+      size_choice: "ratio",
+    };
   }
   go() {
     console.log(this.options);
